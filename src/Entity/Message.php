bb324b341c0ffe0dbfaf2418647c7b248d9346d6
sohaibex <sohaib.elmediouni23@gmail.com>
2023-02-17 00:03:37 +0100

    add entities

---
+++ src/Entity/Message.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MessageRepository::class)]
+class Message
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'messages')]
+    private ?Conversation $Conversation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getConversation(): ?Conversation
+    {
+        return $this->Conversation;
+    }
+
+    public function setConversation(?Conversation $Conversation): self
+    {
+        $this->Conversation = $Conversation;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Participant.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ParticipantRepository::class)]
+class Participant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'participants')]
+    private ?Conversation $convertation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getConvertation(): ?Conversation
+    {
+        return $this->convertation;
+    }
+
+    public function setConvertation(?Conversation $convertation): self
+    {
+        $this->convertation = $convertation;
+
+        return $this;
+    }
+}
