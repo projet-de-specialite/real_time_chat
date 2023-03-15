68e8ca3fd4562ded0100635eb36bea09e8935106
sohaibex <sohaib.elmediouni23@gmail.com>
2023-03-15 14:26:18 +0100

    fix github actions by updating composer.json

---
+++ src/Controller/ConversationController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Conversation;
+use App\Entity\Participant;
+use App\Repository\ConversationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use mysql_xdevapi\Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/conversations', name: 'conversations.')]
+class ConversationController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+    private ConversationRepository $conversationRepository;
+    private $userApiResult;
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        ConversationRepository $conversationRepository
+    )
+    {
+        $this->entityManager = $entityManager;
+        $this->conversationRepository = $conversationRepository;
+    }
+
+
+
+
+    #[Route('/', name: 'newConversations', methods: ['POST'])]
+    public function index(Request $request): JsonResponse
+    {
+
+        $otherUser = $request->get('otherUser', 0);
+        $otherUser = $this->getUserById($otherUser);
+        $otherUser = $this->userRepository->find($otherUser);
+
+        if (is_null($otherUser)) {
+            throw new \Exception("The user was not found");
+        }
+
+        // cannot create a conversation with myself
+        if ($otherUser->getId() === $this->getUser()->getId()) {
+            throw new \Exception("That's deep but you cannot create a conversation with yourself");
+        }
+
+        // Check if conversation already exists
+        $conversation = $this->conversationRepository->findConversationByParticipants(
+            $otherUser->getId(),
+            $this->getUser()->getId()
+        );
+
+        if (count($conversation)) {
+            throw new \Exception("The conversation already exists");
+        }
+
+        $conversation = new Conversation();
+
+        $participant = new Participant();
+        $participant->setUser($this->getUser());
+        $participant->setConversation($conversation);
+
+        $otherParticipant = new Participant();
+        $otherParticipant->setUser($otherUser);
+        $otherParticipant->setConversation($conversation);
+
+        $this->entityManager->getConnection()->beginTransaction();
+
+        try {
+            $this->entityManager->persist($conversation);
+            $this->entityManager->persist($participant);
+            $this->entityManager->persist($otherParticipant);
+
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+        } catch (\Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
+
+        return $this->json([
+            'id' => $conversation->getId()
+        ], Response::HTTP_CREATED);
+    }
+
+    #[Route('/', name: 'getConversations', methods: ['GET'])]
+    public function getConvs(): JsonResponse
+    {
+        $conversations = $this->conversationRepository->findConversationsByUser($this->getUser()->getId());
+
+        return $this->json($conversations);
+    }
+
+
+    public function getUserById(int $id)
+    {
+        try {
+            $httpClient = HttpClient::create();
+            $userResult = $httpClient->request('GET', 'https://api.example.com/data'.$id);
+
+            $content = $userResult->getContent();
+
+            $this->userApiResult = json_decode($content);
+            $response = new Response();
+            $response->setContent(json_encode([
+                'data' => $this->userApiResult,
+                'HTTP' => Response::HTTP_OK
+            ]));
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
+        }
+        catch (\Exception $exception)
+        {
+            throw new \Exception("Erreur: ".$exception->getMessage());
+        }
+    }
+
+    public function getCurrentUser()
+    {
+        try {
+            $httpClient = HttpClient::create();
+            $userResult = $httpClient->request('GET', 'https://api.example.com/data');
+
+            $content = $userResult->getContent();
+
+            $this->userApiResult = json_decode($content);
+            $response = new Response();
+            $response->setContent(json_encode([
+                'data' => $this->userApiResult,
+                'HTTP' => Response::HTTP_OK
+            ]));
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
+        }
+        catch (\Exception $exception)
+        {
+            throw new \Exception("Erreur: ".$exception->getMessage());
+        }
+    }
+
+}
---
+++ src/Controller/MessageController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Conversation;
+use App\Entity\Message;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/messages', name: 'messages.')]
+class MessageController extends AbstractController
+{
+
+    const ATTRIBUTES_TO_SERIALIZE = ['id', 'content', 'createdAt', 'mine'];
+
+    private EntityManagerInterface $entityManager;
+    private MessageRepository $messageRepository;
+
+    public function __construct(EntityManagerInterface $entityManager,
+                                MessageRepository $messageRepository)
+    {
+        $this->entityManager = $entityManager;
+        $this->messageRepository = $messageRepository;
+    }
+
+    #[Route('/{id}', name: 'getMessages', methods: ['GET'])]
+    public function index(Request $request, Conversation $conversation): Response
+    {
+        $this->denyAccessUnlessGranted('view', $conversation);
+
+        $messages = $this->messageRepository->findMessageByConversationId(
+            $conversation->getId()
+        );
+
+        foreach ($messages as $message) {
+            $message->setMine(
+                $message->getUser()->getId() === $this->getUser()->getId()
+                    ? true : false
+            );
+        }
+
+        return $this->json($messages, Response::HTTP_OK, [], [
+            'attributes' => self::ATTRIBUTES_TO_SERIALIZE
+        ]);
+    }
+
+    #[Route('/{id}', name: 'newMessage', methods: ['POST'])]
+    public function newMessage(Request $request, Conversation $conversation): JsonResponse
+    {
+        $user = $this->getUser();
+        $content = $request->get('content', null);
+        $message = new Message();
+        $message->setContent($content);
+        $message->setUser($this->userRepository->findOneBy(['id' => 2]));
+        $message->setMine(true);
+
+        $conversation->addMessage($message);
+        $conversation->setLastMessage($message);
+
+        $this->entityManager->getConnection()->beginTransaction();
+        try {
+            $this->entityManager->persist($message);
+            $this->entityManager->persist($conversation);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+        } catch (\Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
+
+        return $this->json($message, Response::HTTP_CREATED, [], [
+            'attributes' => self::ATTRIBUTES_TO_SERIALIZE
+        ]);
+    }
+}
