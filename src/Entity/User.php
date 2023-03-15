a8d93373f5b6f90cf75eed71099dc62c69734c16
sohaibex <sohaib.elmediouni23@gmail.com>
2023-03-15 14:33:28 +0100

    add user enitity for tests to be deleted after

---
+++ src/Entity/User.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ApiResource]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
