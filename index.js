91674c61e65c275f4cb96e606aa48ea518971f88
sohaibex <sohaib.elmediouni23@gmail.com>
2023-04-10 17:26:56 +0200

    setup the app installing mogoose dotenv helmet and morgan, setup of mongo db secret key

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+
+app.listen(3000, () => {
+    console.log("server running on port 3000");
+})
