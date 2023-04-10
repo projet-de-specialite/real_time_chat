980c761b359d36fc2135e48364511e40cc6d5836
sohaibex <sohaib.elmediouni23@gmail.com>
2023-04-10 17:43:49 +0200

    first hello world

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ const morgan = require("morgan");
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Connected to database!');
+})
+    .catch((error) => {
+        console.error('Failed to connect to database:', error);
+    });
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.get("/", (req, res) => {
+    res.send("Hello World !")
+});
 
 app.listen(3000, () => {
     console.log("server running on port 3000");
