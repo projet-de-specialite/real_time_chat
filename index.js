619633b1c8fe2a0be3f39dede0be242aa8a2c647
sohaibex <sohaib.elmediouni23@gmail.com>
2023-06-11 17:41:13 +0200

    fix ci

--- index.js
+++ index.js
@@ -3,46 +3,56 @@ const app = express();
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const cors = require('cors');
+const cors = require("cors");
 const db = require("./firebase");
-const fs = require('fs');
-
-const client = require('prom-client');
+const client = require("prom-client");
 const collectDefaultMetrics = client.collectDefaultMetrics;
-collectDefaultMetrics({ timeout: 5000 });
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const conversationRoute = require("./routes/conversation");
 const messageRoute = require("./routes/message");
+const server_url = process.env.SERVER_URL;
+collectDefaultMetrics({ timeout: 5000 });
 
 /** Swagger */
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
-
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 //const { auth } = require("firebase-admin");
 
 /** middleware */
 app.use(express.json());
-app.use(helmet());
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                connectSrc: ["'self'", server_url],
+            },
+        },
+    })
+);
+
 app.use(morgan("common"));
 
-var allowedOrigins = [process.env.FRONT_URL, process.env.FRONT_URL, 'http://localhost:3000'];
+var allowedOrigins = [process.env.FRONT_URL];
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    methods: ["GET", "POST"],
-    allowedHeaders: ['*']
-}));
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                var msg =
+                    "The CORS policy for this site does not " +
+                    "allow access from the specified Origin.";
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
+        methods: ["GET", "POST"],
+        allowedHeaders: ["*"],
+    })
+);
 
 /** Routes */
 app.use("/auth", authRoute);
@@ -53,26 +63,26 @@ app.use("/api/messages", messageRoute);
 /** Swagger Setup */
 const swaggerOptions = {
     definition: {
-        openapi: '3.0.0',
+        openapi: "3.0.0",
         info: {
-            title: 'Chat application microservice',
-            version: '1.0.0',
-            description: 'this is real time chat ',
+            title: "Chat application microservice",
+            version: "1.0.0",
+            description: "this is real time chat ",
         },
         servers: [
             {
-                url: 'localhost:5000',
+                url: server_url,
             },
         ],
     },
-    apis: ['./routes/*.js'],
+    apis: ["./routes/*.js"],
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
-app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use((req, res, next) => {
-    const error = new Error('Not Found');
+    const error = new Error("Not Found");
     error.status = 404;
     next(error);
 });
@@ -82,13 +92,13 @@ app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
         error: {
-            message: error.message
-        }
+            message: error.message,
+        },
     });
 });
 
-app.get('/metrics', (req, res) => {
-    res.set('Content-Type', client.register.contentType);
+app.get("/metrics", (req, res) => {
+    res.set("Content-Type", client.register.contentType);
     res.end(client.register.metrics());
 });
 
@@ -97,17 +107,18 @@ const env = process.env.NODE_ENV;
 
 // Load .env file if it exists and we are in a 'development' environment
 
-const envPath = `.env.${env}`;
+if (env !== 'prod') {
+    const envPath = `.env.${env}`;
 
-const result = dotenv.config({ path: envPath });
-if (result.error) {
-    throw result.error;
+    const result = dotenv.config({ path: envPath });
+    if (result.error) {
+        throw result.error;
+    }
 }
-
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(process.env.SERVER_PORT);
-    console.log(`server running on port ${process.env.SERVER_URL}`);
+    console.log(`server running on port ${server_url}`);
 });
 
 module.exports = { app, firestore: db };
