826fcee6f8820ae1ae426a82d2d10bf249b01b60
sohaibex <sohaib.elmediouni23@gmail.com>
2023-06-12 09:52:47 +0200

    fix ci

--- firebase.js
+++ firebase.js
@@ -1,5 +1,21 @@
-const Firestore = require('@google-cloud/firestore');
+const admin = require('firebase-admin');
 
-const firestore = new Firestore();
+let serviceAccount;
+
+try {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+} catch (error) {
+    console.error('Unable to parse FIREBASE_SERVICE_ACCOUNT_KEY:', error);
+}
+
+if (serviceAccount) {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+} else {
+    console.error('Failed to initialize Firebase admin SDK');
+}
+
+const firestore = admin.firestore();
 
 module.exports = firestore;
